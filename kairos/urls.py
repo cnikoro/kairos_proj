091ef9be91e3246655191e75434822aeecfb3306
cnikoro <charlesxavia@gmail.com>
2022-02-11 10:33:44 +0100

    Updated urls.py

--- kairos/urls.py
+++ kairos/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import *
 
 urlpatterns = [
+    path('', home_page, name="home"),
     path('admin/', admin.site.urls),
 ]
